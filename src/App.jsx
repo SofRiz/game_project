9fa95c95914b50ac2a819c81ff2b38461d9ff316
Iván <ivan_nicolas_1993@hotmail.com>
2024-04-24 11:48:38 +0200

    Grid del Home finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import NavigationBar from "./components/NavigationBar"
 import Home from "./pages/Home"
+import Games from "./pages/Games"
 
 function App() {
     return (
         <>
             <NavigationBar />
             <Home />
+            <Games />
         </>
     )
 }
