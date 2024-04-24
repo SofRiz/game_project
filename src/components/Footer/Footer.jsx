dc7d8be9151e33a5a9e1cd5a06a87888d4f9fd10
SitoDP <jesusdprogram@gmail.com>
2024-04-24 11:45:12 +0200

    cambio hover y before footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,6 +14,16 @@ function Footer() {
     return (
         <section className="footer">
             <div class="iconos">
+                {/*<ul>
+                    <li><a href="https://www.facebook.com/UNIVERSAE/"><FaFacebook className="a1" /></a></li>
+                    <li><a href="https://www.instagram.com/_universae/?hl=es"><FaInstagram className="a2" /></a></li>
+                    <li><a href="https://www.youtube.com/c/UNIVERSAE_FP"   > <FaYoutube className="a3" /></a></li>
+                    <li><a href="https://www.twitch.tv/universae_fp"><FaTwitch className="a4" /></a></li>
+                    <li><a href="https://www.linkedin.com/school/universae"><FaLinkedin className="a5" /></a></li>
+                    <li><a href="https://www.tiktok.com/@_universae"><FaTiktok className="a6" /></a></li>
+                    <li><a href="https://twitter.com/_Universae"><BsTwitterX className="a7" /></a></li>
+                    <li><a href="https://universae.com"><FaGoogle className="a8" /></a></li>
+                </ul>*/}
                 <a href="https://www.facebook.com/UNIVERSAE/"><FaFacebook className="a1" /></a>
                 <a href="https://www.instagram.com/_universae/?hl=es"><FaInstagram className="a2" /></a>
                 <a href="https://www.youtube.com/c/UNIVERSAE_FP"   > <FaYoutube className="a3" /></a>
