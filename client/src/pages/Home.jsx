cba30cda7767dea9923b9f33aded8d20435bb7b6
Dreyan Franco <dreyanfranco@gmail.com>
2024-04-25 13:47:06 +0200

    replace of class to classname

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
         try {
             const response = await gameService.getGames()
             setGames(response.data)
+            console.log(response.data)
         } catch (error) {
             console.log("Error fetching games", error)
         }
